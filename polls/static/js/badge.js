389b7933d13a0d3045746be60cd4909f5d893970
Saleh Zaidan <salehzaidan53@gmail.com>
2020-07-12 15:33:28 +0700

    Memberi warna badge yang bergantung pada level

---
+++ polls/static/js/badge.js
@@ -0,0 +1,10 @@
+$('.badge').addClass(function() {
+  switch ($(this).html()) {
+    case 'Sailors': return 'badge-primary'; break;
+    case 'Gunners': return 'badge-secondary'; break;
+    case 'Boatswain': return 'badge-success'; break;
+    case 'Liutenants': return 'badge-danger'; break;
+    case 'Sailing Master': return 'badge-warning'; break;
+    case 'First Mate': return 'badge-info'; break;
+  }
+});
